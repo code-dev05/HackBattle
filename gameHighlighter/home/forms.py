198cc265fdbce36f24cda101e3492a990cc8d8c4
muditrajsade <132217112+muditrajsade@users.noreply.github.com>
2023-09-20 11:31:05 +0530

    chaenges_in_forms_and_views

---
+++ gameHighlighter/home/forms.py
@@ -0,0 +1,6 @@
+
+
+from django import forms
+
+class uplodfileform(forms.Form):
+    file = forms.FileField()
--- gameHighlighter/home/views.py
+++ gameHighlighter/home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from .forms import uplodfileform
+
+user_name=None
 
 # Create your views here.
 def home(request):
@@ -16,6 +19,7 @@ def login_user(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
+            user_name= username
             return redirect('home')
         else:
             print("works")
@@ -29,3 +33,24 @@ def register(request):
 def logout_user(request):
     logout(request)
     return redirect('home')
+
+def file_upload(request):
+    if request.method == "POST":
+        form = uplodfileform(request.POST,request.FILES)
+        if form.is_valid():
+            handle_file(request.FILES["file"])
+
+
+def handle_file(f):
+    import dropbox
+    dbx = dropbox.Dropbox("sl.BmaZxI4b2AB7dyQt6p_UdM7_ZyZSsUyrOlP4ONW-atIApmbas97qOD_G771_SymS_7DcHMREYiyaXEuX5mQO5W4Fswouryp5ZAQCUlXowd60-0YIzz3p83RpJsl01EGH46-l7EONMganrAp71YCyKfU")
+
+
+    
+    file_to = '/hello/'+user_name
+ 
+
+    ft = open(f, 'rb')
+    dbx.files_upload(ft.read(), file_to)
+
+       
