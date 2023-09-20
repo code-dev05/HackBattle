aef3eaf71ff2e4a84b668afb7a4d3e5e46790586
Devansh Agarwal <devansh.agarwal.official@gmail.com>
2023-09-20 15:57:01 +0530

    conquered death

--- gameHighlighter/home/views.py
+++ gameHighlighter/home/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseBadRequest
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
@@ -7,7 +8,14 @@ user_name=None
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    form = uplodfileform()
+    if request.method == "POST":
+        form = uplodfileform(request.POST,request.FILES)
+        if form.is_valid():
+            handle_file(request.FILES['file'])
+        else:
+            form = uplodfileform()
+    return render(request, 'home.html', {'form': form})
 
 def about(request):
     return render(request, 'about.html', {})
@@ -36,21 +44,33 @@ def logout_user(request):
 
 def file_upload(request):
     if request.method == "POST":
-        form = uplodfileform(request.POST,request.FILES)
+        form = uplodfileform(request.POST, request.FILES)
         if form.is_valid():
-            handle_file(request.FILES["file"])
+            if "file" in request.FILES:
+                handle_file(request.FILES["file"])
+            else:
+                return HttpResponseBadRequest("No 'file' key found in uploaded files.")
+        else:
+            return HttpResponseBadRequest("Invalid form data.")
+    
+    # Handle GET requests or other cases where the form is not valid.
+    return HttpResponseBadRequest("Invalid request.")
 
 
 def handle_file(f):
     import dropbox
-    dbx = dropbox.Dropbox("sl.BmaZxI4b2AB7dyQt6p_UdM7_ZyZSsUyrOlP4ONW-atIApmbas97qOD_G771_SymS_7DcHMREYiyaXEuX5mQO5W4Fswouryp5ZAQCUlXowd60-0YIzz3p83RpJsl01EGH46-l7EONMganrAp71YCyKfU")
+    dbx = dropbox.Dropbox("sl.BmYrGRUJSvIosQVs6YtxSlHjA7jByYi8f5Owz-T_MsSKkJgAyK3-RX4yE_gmS1X2kSEoCUcf9YLnX7iI5bGBYoh5F70IfKzwA6BpaTuRlKGICadLkpT_WeoO57mcfh3ZZVz473dcXtzWYvGAlxOgTN4")
 
+   
+   
+    with open('static/upload/' + f.name, 'wb+') as dest:
+        for chunk in f.chunks():
+            dest.write(chunk)
 
+    file_from = 'C:/Users/devan/Data/Hackathon Projects/HackBattle/gameHighlighter/static/upload/' + f.name
+
+    file_to = '/hello/video100.mp4'
     
-    file_to = '/hello/'+user_name
- 
 
-    ft = open(f, 'rb')
+    ft = open(file_from, 'rb')
     dbx.files_upload(ft.read(), file_to)
-
-       
